789bb6a0031c1f48975a8fe4083cd2fa8a4bdf2f
Robert Vuia <robertidfk69@gmail.com>
2022-09-19 15:11:35 +0300

    debugging messages

--- DiscordBot/ImageRandomizer.h
+++ DiscordBot/ImageRandomizer.h
@@ -5,62 +5,68 @@
 #include <string>
 #include <ctime>
 
-struct ImageRandomizer
+namespace kq
 {
-public:
-
-    static ImageRandomizer* getInstance(size_t nCats = 0, size_t nDogs = 0)
+    struct ImageRandomizer
     {
-        if (!m_instance)
-            m_instance = new ImageRandomizer(nCats, nDogs);
-        return m_instance;
-    }
+    public:
 
-    std::string getCat() { return getRandomPhoto(m_Cats, m_nCats); }
-
-    std::string getDog() { return getRandomPhoto(m_Dogs, m_nDogs); }
-
-    std::string getRandomPhoto(std::vector<int>& positions, size_t size)
-    {
-        if (positions.empty())
+        static ImageRandomizer* getInstance(size_t nCats = 0, size_t nDogs = 0)
         {
-            for (auto i = 1; i <= size; ++i)
+            if (!m_instance)
             {
-                positions.push_back(i);
+                std::cout << "Initialized ImageRandomizer...\n";
+                m_instance = new ImageRandomizer(nCats, nDogs);
             }
+            return m_instance;
         }
-        size_t index = rand() % positions.size();
-        std::string filename = std::to_string(positions[index]) + ".jpg";
-        positions.erase(positions.begin() + index);
 
-        return filename;
-    }
+        std::string getCat() { return getRandomPhoto(m_Cats, m_nCats); }
 
-    
+        std::string getDog() { return getRandomPhoto(m_Dogs, m_nDogs); }
 
-private:
+        std::string getRandomPhoto(std::vector<int>& positions, size_t size)
+        {
+            if (positions.empty())
+            {
+                for (auto i = 1; i <= size; ++i)
+                {
+                    positions.push_back(i);
+                }
+            }
+            size_t index = rand() % positions.size();
+            std::string filename = std::to_string(positions[index]) + ".jpg";
+            positions.erase(positions.begin() + index);
 
-    ImageRandomizer(size_t nCats, size_t nDogs)
-        : m_nCats(nCats), m_nDogs(nDogs)
-    {
-        srand(static_cast<int>(time(NULL)));
-        for (auto i = 1; i <= nCats; ++i)
-            m_Cats.push_back(i);
-        for (auto i = 1; i <= nDogs; ++i)
-            m_Dogs.push_back(i);
-    }
+            return filename;
+        }
 
-    static ImageRandomizer* m_instance;
 
-    std::vector<int> m_Cats;
-    std::vector<int> m_Dogs;
 
-    size_t m_nCats;
-    size_t m_nDogs;
+    private:
 
-};
+        ImageRandomizer(size_t nCats, size_t nDogs)
+            : m_nCats(nCats), m_nDogs(nDogs)
+        {
+            srand(static_cast<int>(time(NULL)));
+            for (auto i = 1; i <= nCats; ++i)
+                m_Cats.push_back(i);
+            for (auto i = 1; i <= nDogs; ++i)
+                m_Dogs.push_back(i);
+        }
 
-ImageRandomizer* ImageRandomizer::m_instance = nullptr;
+        static ImageRandomizer* m_instance;
+
+        std::vector<int> m_Cats;
+        std::vector<int> m_Dogs;
+
+        size_t m_nCats;
+        size_t m_nDogs;
+
+    };
+
+    ImageRandomizer* ImageRandomizer::m_instance = nullptr;
+}
 
 
 
